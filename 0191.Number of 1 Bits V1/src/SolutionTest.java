acbc697720208cb53c0ddaa757abafe4d078c454
yuezhongYan <65264957+yuezhongYan@users.noreply.github.com>
2020-05-23 16:14:18 -0400

    Add code for Problem 191

---
+++ 0191.Number of 1 Bits V1/src/SolutionTest.java
@@ -0,0 +1,11 @@
+
+public class SolutionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Solution solution = new Solution();
+        int n = 00000000000000000000000010000000;
+        System.out.println(solution.hammingWeight(n));
+	}
+
+}
