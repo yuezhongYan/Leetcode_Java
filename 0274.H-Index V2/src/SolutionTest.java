9f8598f95b273c3731b4b4c66fbaf497530369c6
yuezhongYan <65264957+yuezhongYan@users.noreply.github.com>
2020-06-24 08:19:45 -0400

    Add code for Problem 274 V2

---
+++ 0274.H-Index V2/src/SolutionTest.java
@@ -0,0 +1,11 @@
+
+public class SolutionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Solution solution = new Solution();
+        int[] citations = {3, 0, 6, 1, 5};
+		System.out.println(solution.hIndex(citations));
+	}
+
+}
